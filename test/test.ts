9d7023e7399276c71a0c3308dd918710fd230264
Phil Kunz <gitlab@philkunz.com>
2019-06-17 10:27:33 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,10 @@
 import { expect, tap } from '@pushrocks/tapbundle';
 
-import * as npmgInstall from '../ts/tools.install';
-import * as smartenv from '@pushrocks/smartenv';
+import * as tools from '../ts/tools.install';
 
-let environment = new smartenv.Smartenv();
 
 tap.test("should install default list globally when parsed 'default' as argument", async () => {
-  await npmgInstall.install('default');
+  await tools.install('default');
 });
 
 tap.start();
