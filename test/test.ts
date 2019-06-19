1252fa8f97f7be914c2e93769a27ce350b21d415
Phil Kunz <gitlab@philkunz.com>
2019-06-19 13:12:10 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,6 @@ import { expect, tap } from '@pushrocks/tapbundle';
 
 import * as tools from '../ts/tools.install';
 
-
 tap.test("should install default list globally when parsed 'default' as argument", async () => {
   await tools.install('default');
 });
